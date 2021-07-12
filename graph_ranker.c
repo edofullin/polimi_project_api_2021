688f2f863c655898a418d8cee3b7876c70cb5512
Edoardo Fullin <edoardo.fullin@outlook.com>
2021-07-12 22:18:51 +0200

    open tests working

--- graph_ranker.c
+++ graph_ranker.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define STR_BUFF_SIZE 4096
+
 void parse_head(char *str, int *N, int *K);
 
 typedef struct
 {
-    int col;
+    int vert_num;
     int *matrix;
 
 } graph_t;
 
-
+typedef struct {
+    u_int64_t score;
+    u_int32_t position;
+} pos_tuple_t;
 
 int minDistance(int dist[], bool sptSet[], int V) {
     // Initialize min value
@@ -26,8 +31,8 @@ int minDistance(int dist[], bool sptSet[], int V) {
     return min_index;
 }
 
-int *dijkstra(graph_t *graph, int src) {
-    int V = graph->col;
+int *dijkstra(const graph_t *graph, int src) {
+    int V = graph->vert_num;
     int *dist = malloc(V * sizeof(int));  // The output array.  dist[i] will hold the shortest
     // distance from src to i
 
@@ -63,86 +68,130 @@ int *dijkstra(graph_t *graph, int src) {
     return dist;
 }
 
-int find_min(u_int64_t *arr, int len) {
-    int min_i = 0;
-    u_int64_t min_val = __UINT64_MAX__;
+u_int64_t compute_score(const graph_t *graph) {
+    int *points = dijkstra(graph, 0);
+    u_int64_t sum = 0;
 
-    for (int i = 0; i < len; i++) {
-        if (arr[i] < min_val) {
-            min_i = i;
-            min_val = arr[i];
+    for (u_int32_t i = 0; i < graph->vert_num; i++) {
+        if (points[i] != INT_MAX)
+            sum += points[i];
+    }
+
+    return sum;
+}
+
+u_int64_t find_max_i(const pos_tuple_t *arr, u_int32_t len) {
+    u_int64_t max = 0;
+    u_int32_t max_i = 0;
+
+    for (u_int32_t i = 0; i < len; i++) {
+        if (arr[i].score > max) {
+            max = arr[i].score;
+            max_i = i;
         }
     }
 
-    return min_i;
+    return max_i;
 }
 
 int main() {
-    int N, K;
-    char buffer[4000];
-    graph_t graphs[100];
-    int next = 0;
-    u_int64_t points[100];
+    u_int32_t N, K;
+    char buffer[STR_BUFF_SIZE];
+    u_int32_t current_num = 0;
+    graph_t g_current;
+    pos_tuple_t *scores;
+
+    int first_topk = 1;
 
     if (scanf("%d", &N) == EOF) {
-        exit(-1);
+        exit(0);
     };
     if (scanf("%d", &K) == EOF) {
-        exit(-1);
+        exit(0);
     };
+
+    g_current.vert_num = N;
+    g_current.matrix = (int *)malloc(N * N * sizeof(int));
+
+    scores = (pos_tuple_t *)malloc(K * sizeof(pos_tuple_t));
+    for (int i = 0; i < K; i++) {
+        scores[i].score = -1;
+        scores[i].position = -1;
+    }
 
     while (1) {
         if (scanf("%s", buffer) == EOF) {
-            exit(-1);
+            exit(0);
         };
         if (!strcmp(buffer, "AggiungiGrafo")) {
             // aggiungi grafo
             // printf("Aggiungo Grafo #%d", next);
-
-            graphs[next].col = N;
-            graphs[next].matrix = malloc(N * N * sizeof(int));
             int index = 0;
             char *tok;
 
             for (int i = 0; i < N; i++) {
                 if (scanf("%s", buffer) == EOF) {
-                    exit(-1);
+                    exit(0);
                 };
                 tok = strtok(buffer, ",");
-                *(graphs[next].matrix + index++) = atoi(tok);
+                *(g_current.matrix + index++) = atoi(tok);
 
                 for (int j = 0; j < N - 1; ++j) {
                     tok = strtok(NULL, ",");
-                    *(graphs[next].matrix + index++) = atoi(tok);
+                    *(g_current.matrix + index++) = atoi(tok);
                 }
             }
 
-            next++;
-        } else if (!strcmp(buffer, "TopK")) {
-            for (int g = 0; g < next; ++g) {
-                points[g] = 0;
+            u_int64_t score = compute_score(&g_current);
 
-                int *dist = dijkstra(&(graphs[g]), 0);
+#ifdef DEBUG
+            printf("score for graph %d is %lu\n", current_num, score);
+#endif
+            int found_flag = 0;
 
-                for (int i = 0; i < graphs->col; ++i) {
-                    if (dist[i] != INT_MAX)
-                        points[g] += dist[i];
+            for (int i = 0; i < K; i++) {
+                if (scores[i].score == -1) {
+#ifdef DEBUG
+                    printf("graph %d put in position %d\n", current_num, i);
+#endif
+                    scores[i].score = score;
+                    scores[i].position = current_num;
+                    found_flag = 1;
+                    break;
+                }
+            }
+
+            if (!found_flag)
+                for (int i = 0; i < K; i++) {
+                    u_int32_t maxi = find_max_i(scores, K);
+
+                    if (score > scores[maxi].score) break;
+#ifdef DEBUG
+                    printf("graph %d put in position %d replacing %d (%lu)\n", current_num, maxi, scores[maxi].position, scores[maxi].score);
+#endif
+                    scores[maxi].score = score;
+                    scores[maxi].position = current_num;
+                    break;
                 }
 
-                free(dist);
+            current_num++;
+
+        } else if (!strcmp(buffer, "TopK")) {
+            int first_flag = 1;
+
+            if (!first_topk) {
+                printf("\n");
             }
 
             for (int i = 0; i < K; i++) {
-                int val = find_min(points, next);
-                points[val] = INT_MAX;
-
-                printf("%d", val);
-                if (i != K - 1) printf(" ");
+                if (scores[i].score == -1) continue;
+                if(!first_flag) printf(" ");
+                printf("%d", scores[i].position);
+                first_flag = 0;
             }
 
-            exit(0);
+            first_topk = 0;
         } else {
-            printf("I'm done, good night.");
             exit(0);
         }
     }
