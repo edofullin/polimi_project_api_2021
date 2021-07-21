03d62620cf7f5336b24bbef5a319abf40154e391
Edoardo Fullin <edoardo.fullin@outlook.com>
2021-07-21 22:59:28 +0200

    removed unused code and heaps

--- graph_ranker.c
+++ graph_ranker.c
@@ -6,13 +6,11 @@
 #include <string.h>
 
 #define STR_BUFF_SIZE 4096
-#define MAX_SCATTER 0.3f
 
 typedef struct
 {
     int vert_num;
     uint32_t *matrix;
-    float scatter_ratio;
 
 } graph_t;
 
@@ -20,18 +18,6 @@ typedef struct {
     uint64_t score;
     uint32_t position;
 } pos_tuple_t;
-
-typedef struct {
-    uint64_t distance;
-    uint64_t vert;
-} minheap_node_t;
-
-typedef struct {
-    uint32_t maxsize;
-    uint32_t size;
-    minheap_node_t **data;
-    uint32_t *node_pos;
-} minheap_t;
 
 typedef struct node {
     uint64_t score;
@@ -53,48 +39,15 @@ void swap(void **x, void **y) {
 }
 
 uint32_t my_stoi(const char *str, uint32_t len);
-uint64_t compute_score(const graph_t *graph, minheap_t *heap, uint64_t *points);
-void min_heapify(minheap_t *heap, uint32_t index);
-minheap_node_t *heap_min_node(minheap_t *heap);
-void update_key(minheap_t *heap, uint32_t v, uint32_t dist);
-int in_heap(minheap_t *heap, int v);
-void dijkstra_h(const graph_t *graph, int src, uint64_t *distances, minheap_t *heap);
+uint64_t compute_score(graph_t *graph, uint64_t *points);
 scores_list_node_t *make_node(uint32_t position, uint64_t score, scores_list_node_t *next, scores_list_node_t *prev);
 void list_insert_in_order_capped(scores_list_t *list, uint64_t score, uint32_t position, uint32_t cap);
 void destroy_list(scores_list_t *list);
 void destroy_list_from(scores_list_t *list, scores_list_node_t *from);
+uint64_t matr_distance(uint32_t num, u_int64_t *dist, bool *visited);
+void matr_dijkstra(graph_t *graph, uint32_t start, uint64_t *dist);
 
-u_int64_t get_distance(u_int64_t dist[], bool sptSet[], int V) {
-    uint64_t min = UINT64_MAX, min_index = -1;
-
-    for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-
-    return min_index;
-}
-
-void dijkstra(const graph_t *graph, int src, u_int64_t *dist) {
-    int V = graph->vert_num;
-
-    bool sptSet[V];
-
-    for (int i = 0; i < V; i++)
-        dist[i] = UINT64_MAX, sptSet[i] = false;
-
-    dist[src] = 0;
-
-    for (int count = 0; count < V - 1; count++) {
-        u_int64_t u = get_distance(dist, sptSet, V);
-
-        sptSet[u] = true;
-
-        for (int v = 0; v < V; v++) {
-            if (!sptSet[v] && graph->matrix[u * V + v] && dist[u] != UINT64_MAX && dist[u] + graph->matrix[u * V + v] < dist[v])
-                dist[v] = dist[u] + graph->matrix[u * V + v];
-        }
-    }
-}
+#ifdef DEBUG
 
 void print_list(scores_list_t *list) {
     scores_list_node_t *it = list->head;
@@ -106,6 +59,8 @@ void print_list(scores_list_t *list) {
     printf("\n");
 }
 
+#endif
+
 int main() {
     char buffer[STR_BUFF_SIZE];
     int exit_flag = 0;
@@ -115,10 +70,9 @@ int main() {
     uint32_t current_num = 0;
     graph_t g_current;
 
-    minheap_t *dheap = (minheap_t *)malloc(sizeof(minheap_t));
     uint64_t *points;
 
-    scores_list_t score_list = { .head = NULL, .tail = NULL, .length = 0};
+    scores_list_t score_list = {.head = NULL, .tail = NULL, .length = 0};
 
     int first_topk = 1;
 
@@ -127,13 +81,6 @@ int main() {
     }
     if (scanf("%d", &K) == EOF) {
         exit(0);
-    }
-
-    dheap->data = (minheap_node_t **)malloc(N * sizeof(minheap_node_t));
-    dheap->node_pos = (uint32_t *)malloc(N * sizeof(uint32_t));
-
-    for (uint32_t i = 0; i < N; i++) {
-        dheap->data[i] = (minheap_node_t *)malloc(sizeof(minheap_node_t));
     }
 
     points = (uint64_t *)malloc(N * sizeof(uint64_t));
@@ -149,7 +96,6 @@ int main() {
             uint32_t index = 0;
             uint32_t read;
             char *it_start, *it_end;
-            uint32_t zeros = 0;
 
             for (int i = 0; i < N; i++) {
                 if (scanf("%s%n", buffer, &read) == EOF) {
@@ -164,12 +110,6 @@ int main() {
                     if (*it_end == ',' || read == 1) {
                         uint32_t num = my_stoi(it_start, it_end - it_start);
 
-                        if (!num) zeros++;
-
-#ifdef VERBOSE
-                        printf("read num %d adding to matrix in pos %d\n", num, index);
-#endif
-
                         g_current.matrix[index] = num;
 
                         it_start = it_end + 1;
@@ -182,17 +122,14 @@ int main() {
             }
 
             if (exit_flag) break;
-            
-            g_current.scatter_ratio = (float)zeros / (N*N);
 
-            uint64_t score = compute_score(&g_current, dheap, points);
+            uint64_t score = compute_score(&g_current, points);
 
             list_insert_in_order_capped(&score_list, score, current_num, K);
 
 #ifdef DEBUG
-            printf("score for graph %d is %lu scatter %f using %s\n", current_num, score, g_current.scatter_ratio, g_current.scatter_ratio <= MAX_SCATTER ? "heap" : "matrix");
-            printf("list length is %d\n", score_list.length);
-            printf("list tail is pointing %d(%ld)\n", score_list.tail->position, score_list.tail->score);
+            printf("score for graph %d is %lu\n", current_num, score);
+            printf("list tail is pointing %d(%ld) length is %d\n", score_list.tail->position, score_list.tail->score, score_list.length);
             print_list(&score_list);
 #endif
 
@@ -219,10 +156,8 @@ int main() {
         }
     }
 
+    // usano tempo, in ogni caso la mamoria viene liberata in uscita
     // free(points);
-    // free(dheap->node_pos);
-    // free(dheap->data);
-    // free(dheap);
     // free(g_current.matrix);
     // destroy_list(&score_list);
     printf("\n");
@@ -238,129 +173,16 @@ inline uint32_t my_stoi(const char *str, uint32_t len) {
     return val;
 }
 
-uint64_t compute_score(const graph_t *graph, minheap_t *heap, uint64_t *points) {
+uint64_t compute_score(graph_t *graph, uint64_t *points) {
     uint64_t sum = 0;
 
-    if (graph->scatter_ratio <= MAX_SCATTER)
-        dijkstra_h(graph, 0, points, heap);
-    else
-        dijkstra(graph, 0, points);
+    matr_dijkstra(graph, 0, points);  // uso dijkstra con le matrici, i grafi non sono sparsi in genere.
 
     for (uint32_t i = 0; i < graph->vert_num; i++) {
         if (points[i] != UINT64_MAX) sum += points[i];
     }
 
     return sum;
-}
-
-void min_heapify(minheap_t *heap, uint32_t index) {
-    uint32_t left = 2 * index + 1, right = 2 * index + 2;
-    uint32_t posmin;
-
-    if (left < heap->size && heap->data[left]->distance < heap->data[index]->distance)
-        posmin = left;
-    else
-        posmin = index;
-
-    if (right < heap->size && heap->data[right]->distance < heap->data[posmin]->distance)
-        posmin = right;
-
-    if (posmin != index) {
-        minheap_node_t *min_node = heap->data[posmin];
-        minheap_node_t *index_node = heap->data[index];
-
-        heap->node_pos[min_node->vert] = index;
-        heap->node_pos[index_node->vert] = posmin;
-
-        swap((void **)&heap->data[posmin], (void **)&heap->data[index]);
-
-        min_heapify(heap, posmin);
-    }
-}
-
-minheap_node_t *heap_min_node(minheap_t *heap) {
-    minheap_node_t *max = heap->data[0];
-
-    minheap_node_t *end = heap->data[heap->size - 1];
-    heap->data[0] = end;
-
-    heap->node_pos[max->vert] = heap->size - 1;
-    heap->node_pos[end->vert] = 0;
-
-    heap->data[heap->size - 1] = max;
-
-    heap->size--;
-
-    min_heapify(heap, 0);
-
-    return max;
-}
-
-void update_key(minheap_t *heap, uint32_t v, uint32_t dist) {
-    uint32_t i = heap->node_pos[v];
-    heap->data[i]->distance = dist;
-
-    while (i && heap->data[i]->distance < heap->data[(i - 1) / 2]->distance) {
-        heap->node_pos[heap->data[i]->vert] =
-            (i - 1) / 2;
-        heap->node_pos[heap->data[(i - 1) / 2]->vert] = i;
-        swap((void **)&heap->data[i],
-             (void **)&heap->data[(i - 1) / 2]);
-
-        i = (i - 1) / 2;
-    }
-}
-
-int in_heap(minheap_t *heap, int v) {
-    return heap->node_pos[v] < heap->size;
-}
-
-void dijkstra_h(const graph_t *graph, int src, uint64_t *distances, minheap_t *heap) {
-    uint32_t verts = graph->vert_num;
-
-    heap->maxsize = verts;
-    heap->size = 0;
-
-    for (uint32_t i = 0; i < verts; i++) {
-        // minheap_node_t *node = (minheap_node_t *)malloc(sizeof(minheap_node_t));
-
-        distances[i] = UINT64_MAX;
-        heap->data[i]->distance = UINT64_MAX;
-        heap->data[i]->vert = i;
-
-        heap->node_pos[i] = i;
-    }
-
-    heap->data[src]->distance = 0;
-    heap->data[src]->distance = distances[src];
-
-    heap->node_pos[src] = src;
-    distances[src] = 0;
-
-    update_key(heap, src, distances[src]);
-
-    heap->size = verts;
-
-    while (heap->size > 0) {
-        minheap_node_t *min = heap_min_node(heap);
-
-        uint32_t *matr_pointer = graph->matrix + (min->vert * verts);
-
-        for (uint32_t i = 0; i < verts; ++i) {
-            uint32_t arc = *(matr_pointer + i);
-
-            if (!arc) continue;
-
-            if (in_heap(heap, i) && distances[min->vert] != UINT64_MAX && arc + distances[min->vert] < distances[i]) {
-                uint32_t newdist = distances[min->vert] + arc;
-                distances[i] = newdist == UINT32_MAX || newdist == UINT64_MAX ? 0 : newdist;
-
-                update_key(heap, i, distances[i]);
-            }
-        }
-
-        // free(min);
-    }
 }
 
 scores_list_node_t *make_node(uint32_t position, uint64_t score, scores_list_node_t *next, scores_list_node_t *prev) {
@@ -463,5 +285,39 @@ void destroy_list_from(scores_list_t *list, scores_list_node_t *from) {
         free(it);
         list->length--;
         it = next;
+    }
+}
+
+uint64_t matr_distance(uint32_t num, u_int64_t *dist, bool *visited) {
+    uint64_t min = UINT64_MAX, min_index = -1;
+
+    for (int i = 0; i < num; i++)
+        if (!visited[i] && dist[i] <= min)
+            min = dist[i], min_index = i;
+
+    return min_index;
+}
+
+void matr_dijkstra(graph_t *graph, uint32_t start, uint64_t *dist) {
+    uint32_t N = graph->vert_num;
+
+    bool visited[N];
+
+    for (int i = 0; i < N; i++)
+        dist[i] = UINT64_MAX, visited[i] = false;
+
+    dist[start] = 0;
+
+    for (int count = 0; count < N - 1; count++) {
+        u_int64_t u = matr_distance(N, dist, visited);
+
+        visited[u] = true;
+
+        for (int v = 0; v < N; v++) {
+            // non vistato con distanza non nulla non già massima
+            // e non maggiore di quella già eventualmente calcolata per altri nodi successivi
+            if (!visited[v] && graph->matrix[u * N + v] && dist[u] != UINT64_MAX && dist[u] + graph->matrix[u * N + v] < dist[v])
+                dist[v] = dist[u] + graph->matrix[u * N + v];
+        }
     }
 }
